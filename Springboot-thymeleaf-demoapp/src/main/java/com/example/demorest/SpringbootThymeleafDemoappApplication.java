73a66e4e1ddaa68c06eefc9540f01e2aa11ae690
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-05-04 14:47:20 +0530

    Employee App

---
+++ Springboot-thymeleaf-demoapp/src/main/java/com/example/demorest/SpringbootThymeleafDemoappApplication.java
@@ -0,0 +1,14 @@
+package com.example.demorest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafDemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafDemoappApplication.class, args);
+	}
+
+}
