0c7e1fd45737a9f76b59e7acce8c2fafed6a22f1
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-05-05 11:33:04 +0530

    Model Mapper

---
+++ Springboot-thymeleaf-demoapp/src/main/java/modelMapper/TestModelMapper.java
@@ -0,0 +1,17 @@
+package modelMapper;
+
+import org.modelmapper.ModelMapper;
+
+public class TestModelMapper {
+	public static void main(String[] args) {
+		ModelMapper modelMapper = new ModelMapper();
+		Order order = new Order(new Customer(new Name("Mrudula","Naik")),
+								new Address("new street", "new city"));
+		
+		OrderDTO orderDTO = modelMapper.map(order, OrderDTO.class); 
+		System.out.println(orderDTO.toString());
+		
+	}
+
+}
+
